2c84b9ff43022f3b4aa168d44e0067792bbd17f4
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2020-11-13 20:12:40 +0100

    wip

--- src/modern-async.js
+++ src/modern-async.js
@@ -1,4 +1,6 @@
 
+import assert from 'assert'
+
 /**
  * Waits the given amount of time
  *
@@ -6,6 +8,7 @@
  * @returns {Promise} A promise that will be resolved after the given amount of time
  */
 async function wait (amount) {
+  assert(typeof amount === 'number')
   return new Promise((resolve) => {
     setTimeout(resolve, amount)
   })
