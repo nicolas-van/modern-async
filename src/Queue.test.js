a55e3aa955984c709050d7b6d7478463b43bd4d5
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2020-11-26 22:34:11 +0100

    Added test

--- src/Queue.test.js
+++ src/Queue.test.js
@@ -161,6 +161,50 @@ test('Queue infinity', async () => {
   expect(callCount[5]).toBe(1)
 })
 
+test('Queue infinity race', async () => {
+  const unit = 30
+  const start = new Date().getTime()
+  const queue = new Queue(Number.POSITIVE_INFINITY)
+  expect(queue.concurrency).toBe(Number.POSITIVE_INFINITY)
+  expect(queue.running).toBe(0)
+  expect(queue.pending).toBe(0)
+  const promises = []
+  const callCount = {}
+  for (const x of _.range(6)) {
+    callCount[x] = 0
+    const p = queue.exec(async () => {
+      callCount[x] += 1
+      await preciseWait(unit)
+    })
+    expect(p).toBeInstanceOf(Promise)
+    promises.push(p)
+  }
+  expect(promises.length).toBe(6)
+  expect(queue.running).toBe(6)
+  expect(queue.pending).toBe(0)
+  expect(callCount[0]).toBe(1)
+  expect(callCount[1]).toBe(1)
+  expect(callCount[2]).toBe(1)
+  expect(callCount[3]).toBe(1)
+  expect(callCount[4]).toBe(1)
+  expect(callCount[5]).toBe(1)
+  await Promise.race(promises)
+  expect(queue.running).toBe(5)
+  expect(queue.pending).toBe(0)
+  await Promise.all(promises)
+  expect(queue.running).toBe(0)
+  expect(queue.pending).toBe(0)
+  const now = new Date().getTime()
+  expect(now - start).toBeGreaterThanOrEqual(unit * 1)
+  expect(now - start).toBeLessThan(unit * 1 * 3)
+  expect(callCount[0]).toBe(1)
+  expect(callCount[1]).toBe(1)
+  expect(callCount[2]).toBe(1)
+  expect(callCount[3]).toBe(1)
+  expect(callCount[4]).toBe(1)
+  expect(callCount[5]).toBe(1)
+})
+
 test('Queue throws', async () => {
   const unit = 30
   const start = new Date().getTime()
