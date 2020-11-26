7e27c4a1849c19f5c8be4e73f12e4d26408d7c73
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2020-11-26 20:13:45 +0100

    doc

--- src/cancellableWait.js
+++ src/cancellableWait.js
@@ -5,7 +5,9 @@ import CancelledError from './CancelledError'
 /**
  * Waits a given amount of time. This function returns both a promise and cancel function in
  * order to cancel the wait time if necessary. If cancelled, the promise will be rejected
- * with a CancelError.
+ * with a CancelError. This function uses setTimeout() internally and has the same
+ * behavior, notably that it could resolve after the asked time (depending on other tasks running)
+ * or a few milliseconds before.
  *
  * @param {number} amount An amount of time in milliseconds
  * @returns {Array} A tuple of two objects:
--- src/wait.js
+++ src/wait.js
@@ -2,7 +2,9 @@
 import cancellableWait from './cancellableWait'
 
 /**
- * Waits a given amount of time.
+ * Waits a given amount of time. This function uses setTimeout() internally and has the same
+ * behavior, notably that it could resolve after the asked time (depending on other tasks running)
+ * or a few milliseconds before.
  *
  * @param {number} amount An amount of time in milliseconds
  * @returns {Promise} A promise that will be resolved after the given amount of time has passed.
