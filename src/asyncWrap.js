88b6e9bc77b399f8ee166b88af3a85e05a3f490c
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2020-11-26 22:00:52 +0100

    fixed doc

--- src/asyncWrap.js
+++ src/asyncWrap.js
@@ -3,10 +3,10 @@
  * Wraps a function call that may not be asynchronous in a function that
  * is guaranted to be async. This is a stricter version of calling a
  * function and wrapping its result using Promise.resolve() as the new function also
- * handles the case where the true function throws an exception.
+ * handles the case where the original function throws an exception.
  *
- * @param {Function} fct A function that will immediately be called without arguments.
- * @returns {Function} A promise that will be terminated using the result of fct.
+ * @param {Function} fct The function to wrap.
+ * @returns {Function} The wrapped function.
  */
 export default function asyncWrap (fct) {
   return async function () {
