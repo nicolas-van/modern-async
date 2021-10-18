e78f378fd0c9ba0c088e41ae599cee0d72d0492e
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2021-10-19 01:33:29 +0200

    Added tests

--- src/asyncGeneratorMap.test.mjs
+++ src/asyncGeneratorMap.test.mjs
@@ -48,3 +48,92 @@ test('asyncGeneratorMap infinity all resolve in micro tasks', async () => {
   await del
   expect(finished).toBe(true)
 })
+
+test('asyncGeneratorMap same queue three levels concurrency 1', async () => {
+  const callList = []
+  const queue = new Queue(1)
+  const gen1 = asyncGeneratorMap(range(3), async (x, i) => {
+    callList.push([0, i])
+    return x * 2
+  }, queue)
+  const gen2 = asyncGeneratorMap(gen1, async (x, i) => {
+    callList.push([1, i])
+    return x * 2
+  }, queue)
+  const gen3 = asyncGeneratorMap(gen2, async (x, i) => {
+    callList.push([2, i])
+    return x * 2
+  }, queue)
+  const p = (async () => {
+    const res = []
+    for await (const el of gen3) {
+      res.push(el)
+    }
+    return res
+  })()
+  const res = await p
+  expect(res).toStrictEqual([0, 8, 16])
+  expect(callList).toStrictEqual([
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [0, 1],
+    [1, 1],
+    [2, 1],
+    [0, 2],
+    [1, 2],
+    [2, 2]
+  ])
+})
+
+test('asyncGeneratorMap same queue three levels concurrency 2', async () => {
+  const callList = []
+  const queue = new Queue(2)
+  const gen1 = asyncGeneratorMap(range(3), async (x, i) => {
+    callList.push([0, i])
+    return x * 2
+  }, queue)
+  const gen2 = asyncGeneratorMap(gen1, async (x, i) => {
+    callList.push([1, i])
+    return x * 2
+  }, queue)
+  const gen3 = asyncGeneratorMap(gen2, async (x, i) => {
+    callList.push([2, i])
+    return x * 2
+  }, queue)
+  const p = (async () => {
+    const res = []
+    for await (const el of gen3) {
+      res.push(el)
+    }
+    return res
+  })()
+  const res = await p
+  expect(res).toStrictEqual([0, 8, 16])
+})
+
+test('asyncGeneratorMap same queue three levels concurrency infinity', async () => {
+  const callList = []
+  const queue = new Queue(Number.POSITIVE_INFINITY)
+  const gen1 = asyncGeneratorMap(range(3), async (x, i) => {
+    callList.push([0, i])
+    return x * 2
+  }, queue)
+  const gen2 = asyncGeneratorMap(gen1, async (x, i) => {
+    callList.push([1, i])
+    return x * 2
+  }, queue)
+  const gen3 = asyncGeneratorMap(gen2, async (x, i) => {
+    callList.push([2, i])
+    return x * 2
+  }, queue)
+  const p = (async () => {
+    const res = []
+    for await (const el of gen3) {
+      res.push(el)
+    }
+    return res
+  })()
+  const res = await p
+  expect(res).toStrictEqual([0, 8, 16])
+})
