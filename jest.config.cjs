8a37409c34738ce7b866ed5c7e340c6088198e77
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2021-11-03 23:35:35 +0100

    Simplified config

--- jest.config.cjs
+++ jest.config.cjs
@@ -170,7 +170,7 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: {},
+  // transform: {},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will
   // skip transformation
